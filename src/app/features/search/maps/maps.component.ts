d78352150009affe4b2794ee1310d1be2a96cee1
pellyadolfo <pellyadolfo@yahoo.es>
2025-03-26 21:15:35 +0100

    added marker colors

--- src/app/features/search/maps/maps.component.ts
+++ src/app/features/search/maps/maps.component.ts
@@ -131,15 +131,20 @@ export class AngularGoogleMapsComponent {
 								this.selectorsCopy.selectedServiceCode == 'AD' ? "M1 6V15H6V11C6 9.89543 6.89543 9 8 9C9.10457 9 10 9.89543 10 11V15H15V6L8 0L1 6Z" :
 								"",
 					strokeColor: "#000000",
-					fillColor:  "#FF0000",
+					fillColor:  merchant.score > 4.4 ? "#d5e197" : 
+											merchant.score > 3.9 ? "#eaf2d7" : 
+											merchant.score > 3.6 ? "#ffdbc2" : 
+											merchant.score > 3 ? "#f2b4a3" : 
+											merchant.score > 0 ? "#f38989" : 
+											"#ffffff",
 					fillOpacity: 1.0,
 					scale: 1
 				},
-        label: {
+        /*label: {
           text: merchant.num.length > 0 ? merchant.num : '-',
           className: 'circle-label',
           fontSize: '12px'
-        },
+        },*/
       },
       markerWindowInfo: {
         html: '<div>' + (merchant.web ? '<a href=\"' + merchant.web + '\" target=\"_blank\">' + merchant.name + '</a>' : merchant.name) + '</div>'
@@ -155,6 +160,7 @@ export class AngularGoogleMapsComponent {
 							+ '</p>'
       }
     } as IMapCircle);
+
   }
 	ngAfterViewInit() {
 		// map available on ngAfterViewInit

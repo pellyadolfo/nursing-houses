165f21018e60d9f9987c2621ba5dfe3c90bdd3cf
pellyadolfo <pellyadolfo@yahoo.es>
2025-03-29 17:49:52 +0100

    added data

--- src/app/services/dataImportsService.ts
+++ src/app/services/dataImportsService.ts
@@ -6,11 +6,13 @@ import { Injectable } from '@angular/core';
 export class DataImportsService {
 
 	loadData (country: string, city: string) {
+		if (country === 'ES' && city === 'AV')
+			return import('../../assets/data/ES_AV.json').then(m=> m)
+		if (country === 'ES' && city === 'GO')
+			return import('../../assets/data/ES_GO.json').then(m=> m)
 		if (country === 'ES' && city === 'GI')
 			return import('../../assets/data/ES_GI.json').then(m=> m)
-		else if (country === 'ES' && city === 'AV')
-			return import('../../assets/data/ES_AV.json').then(m=> m)
-		else if (country === 'MX' && city === 'CV')
+		if (country === 'MX' && city === 'CV')
 			return import('../../assets/data/MX_CV.json').then(m=> m)
 
 		return import('../../assets/data/ES_GI.json').then(m=> m)

ae53a071903df0531a7c74d05aa1cdbeb56ba5d4
pellyadolfo <pellyadolfo@yahoo.es>
2025-03-30 19:37:34 +0200

    updated data

--- src/app/services/dataImportsService.ts
+++ src/app/services/dataImportsService.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class DataImportsService {
 
+	// https://socialasturias.asturias.es/documents/38532/0/Acreditados+para+el+portal+enero2025+%281%29.pdf/c5f9c2d5-df84-2003-df96-487007abb500?t=1738916712073
 	loadData (country: string, city: string) {
 		return import('../../assets/data/' + country + '_' + city + '.json').then(m=> m)
 	} 
